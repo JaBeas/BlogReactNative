5afff6aeb0e19663bd6442f8854964b962ec0bb5
Jaikar Beasley <jaikarbeasley@gmail.com>
2022-01-16 22:53:57 -0800

    Added Edit Screen

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,15 +1,16 @@
-import createDataContext from "./createDataContext";
+import createDataContext from './createDataContext';
 
 const blogReducer = (state, action) => {
   switch (action.type) {
-    case "delete_blogpost":
+    case 'delete_blogpost':
       return state.filter((blogPost) => blogPost.id !== action.payload);
-    case "add_blogpost":
+    case 'add_blogpost':
       return [
         ...state,
         {
           id: Math.floor(Math.random() * 99999),
-          title: `Blog Post #${state.length + 1}`,
+          title: action.payload.content,
+          content: action.payload.content,
         },
       ];
     default:
@@ -18,19 +19,23 @@ const blogReducer = (state, action) => {
 };
 
 const addBlogPost = (dispatch) => {
-  return () => {
-    dispatch({ type: "add_blogpost" });
+  return (title, content, callback) => {
+    dispatch({
+      type: 'add_blogpost',
+      payload: { title, content },
+    });
+    callback();
   };
 };
 
 const deleteBlogPost = (dispatch) => {
   return (id) => {
-    dispatch({ type: "delete_blogpost", payload: id });
+    dispatch({ type: 'delete_blogpost', payload: id });
   };
 };
 
 export const { Context, Provider } = createDataContext(
   blogReducer,
   { addBlogPost, deleteBlogPost },
-  []
+  [{ title: 'TEST POST', content: 'TEST CONTENT', id: 1 }]
 );
