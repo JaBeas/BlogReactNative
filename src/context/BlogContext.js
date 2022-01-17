d9fbabe4d446a96747bc07d2c3297ab9fa8c1823
Jaikar Beasley <jaikarbeasley@gmail.com>
2022-01-16 23:54:43 -0800

    destructured logic and added navigation logic

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -2,6 +2,10 @@ import createDataContext from './createDataContext';
 
 const blogReducer = (state, action) => {
   switch (action.type) {
+    case 'edit_blogpost':
+      return state.map((blogPost) => {
+        return blogPost.id === action.payload.id ? action.payload : blogPost;
+      });
     case 'delete_blogpost':
       return state.filter((blogPost) => blogPost.id !== action.payload);
     case 'add_blogpost':
@@ -9,7 +13,7 @@ const blogReducer = (state, action) => {
         ...state,
         {
           id: Math.floor(Math.random() * 99999),
-          title: action.payload.content,
+          title: action.payload.title,
           content: action.payload.content,
         },
       ];
@@ -24,7 +28,9 @@ const addBlogPost = (dispatch) => {
       type: 'add_blogpost',
       payload: { title, content },
     });
-    callback();
+    if (callback) {
+      callback();
+    }
   };
 };
 
@@ -34,8 +40,17 @@ const deleteBlogPost = (dispatch) => {
   };
 };
 
+const editBlogPost = (dispatch) => {
+  return (id, title, content, callback) => {
+    dispatch({ type: 'edit_blogpost', payload: { id, title, content } });
+    if (callback) {
+      callback();
+    }
+  };
+};
+
 export const { Context, Provider } = createDataContext(
   blogReducer,
-  { addBlogPost, deleteBlogPost },
+  { addBlogPost, deleteBlogPost, editBlogPost },
   [{ title: 'TEST POST', content: 'TEST CONTENT', id: 1 }]
 );
--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import { View, Text, StyleSheet, TextInput } from 'react-native';
-import { useContext, useState } from 'react/cjs/react.development';
+import { StyleSheet } from 'react-native';
+import { useContext } from 'react/cjs/react.development';
 import { Context } from '../context/BlogContext';
+import BlogPostForm from '../components/BlogPostForm';
 
 function EditScreen({ navigation }) {
-  const { state } = useContext(Context);
+  const id = navigation.getParam('id');
+  const { state, editBlogPost } = useContext(Context);
 
-  const blogPost = state.find(
-    (blogPost) => blogPost.id === navigation.getParam('id')
-  );
-
-  const [title, setTitle] = useState(blogPost.title);
-  const [content, setContent] = useState(blogPost.content);
+  const blogPost = state.find((blogPost) => blogPost.id === id);
 
   return (
-    <View>
-      <Text>Edit Title: </Text>
-      <TextInput
-        value={title}
-        onChangeText={(newTitle) => setTitle(newTitle)}
-      />
-    </View>
+    <BlogPostForm
+      initialValues={{ title: blogPost.title, content: blogPost.content }}
+      onSubmit={(title, content) => {
+        editBlogPost(id, title, content, () => navigation.pop());
+      }}
+    />
   );
 }
 
