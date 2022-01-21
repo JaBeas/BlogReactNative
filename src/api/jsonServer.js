ec7e1a92de301378607a0e10ecca5b8e322d41bb
Jaikar Beasley <jaikarbeasley@gmail.com>
2022-01-21 03:19:58 -0800

    added add, edit, delete functions with server

--- src/api/jsonServer.js
+++ src/api/jsonServer.js
@@ -2,5 +2,7 @@ import axios from 'axios';
 
 export default axios.create({
   // baseURL must be updated manually every 8 hours
-  baseURL: ' http://2217-73-162-253-129.ngrok.io',
+  //npm run db
+  // npm run tunnel
+  baseURL: 'https://c305-73-162-253-129.ngrok.io',
 });
--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -11,15 +11,6 @@ const blogReducer = (state, action) => {
       });
     case 'delete_blogpost':
       return state.filter((blogPost) => blogPost.id !== action.payload);
-    case 'add_blogpost':
-      return [
-        ...state,
-        {
-          id: Math.floor(Math.random() * 99999),
-          title: action.payload.title,
-          content: action.payload.content,
-        },
-      ];
     default:
       return state;
   }
@@ -29,16 +20,13 @@ const getBlogPosts = (dispatch) => {
   return async () => {
     const response = await jsonServer.get('/blogposts');
 
-    dispatch({ type: 'get_blogposts', playload: response.data });
+    dispatch({ type: 'get_blogposts', payload: response.data });
   };
 };
 
 const addBlogPost = (dispatch) => {
-  return (title, content, callback) => {
-    dispatch({
-      type: 'add_blogpost',
-      payload: { title, content },
-    });
+  return async (title, content, callback) => {
+    await jsonServer.post('/blogposts', { title, content });
     if (callback) {
       callback();
     }
@@ -46,13 +34,16 @@ const addBlogPost = (dispatch) => {
 };
 
 const deleteBlogPost = (dispatch) => {
-  return (id) => {
+  return async (id) => {
+    await jsonServer.delete(`/blogposts/${id}`);
     dispatch({ type: 'delete_blogpost', payload: id });
   };
 };
 
 const editBlogPost = (dispatch) => {
-  return (id, title, content, callback) => {
+  return async (id, title, content, callback) => {
+    await jsonServer.put(`/blogposts/${id}`, { title, content });
+
     dispatch({ type: 'edit_blogpost', payload: { id, title, content } });
     if (callback) {
       callback();
